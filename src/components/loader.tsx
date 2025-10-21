000130de652fe3051cb81ed7e9d4706760297c7c
roso1nik <103901662+roso1nik@users.noreply.github.com>
2025-10-21 18:18:04 +0300

    add loader & logout

---
+++ src/components/loader.tsx
@@ -0,0 +1,3 @@
+export const Loader = () => {
+    return <div className="mx-auto h-32 w-32 animate-spin rounded-full border-b-2 border-[#8A2BE2]"></div>
+}
--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable react-perf/jsx-no-new-function-as-prop */
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { useLogout } from '@/entities/auth/api/use-logout'
 import { ROUTES } from '@/shared/router'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Cookies from 'js-cookie'
+import { GLOBAL_DICTIONARY } from '@/shared/config'
 
 export const Header = () => {
+    const { push } = useRouter()
+
+    const refreshToken = Cookies.get(GLOBAL_DICTIONARY.REFRESH_TOKEN)
+    const { mutate: logout, isPending: isLoadingLogout } = useLogout()
+
     return (
         <div className="border-primary/50 bg-background/50 flex w-full flex-row items-center justify-between rounded-full border p-4 backdrop-blur">
             <div>
@@ -13,11 +23,20 @@ export const Header = () => {
                 </Link>
             </div>
             <div className="flex flex-row gap-1">
-                <Link href={ROUTES.AUTH_PAGE}>
-                    <Button variant="ghost" className="cursor-target">
-                        Логин
-                    </Button>
-                </Link>
+                <Button
+                    variant="ghost"
+                    className="cursor-target"
+                    onClick={() => {
+                        if (refreshToken) {
+                            logout({ refreshToken: refreshToken })
+                        } else {
+                            push(ROUTES.AUTH_PAGE)
+                        }
+                    }}
+                    loading={isLoadingLogout}
+                >
+                    {refreshToken ? 'Выйти' : 'Логин'}
+                </Button>
             </div>
         </div>
     )
